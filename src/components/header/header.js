7a649cbd28f27cd0f946aa9f6e49543a6502983b
Lautaro <lautyylazzerii@gmail.com>
2022-01-11 21:36:17 -0300

    header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -27,6 +27,7 @@ export default function Header({ className }) {
                 </Link>
               ))}
             </Flex>
+            
             <Button className='donate__btn' variant='secondary' aria-label='Get Started' >
               Get Started
             </Button>
