16463b49ca001506e421dfcb094799644c21a0bb
Lautaro <lautyylazzerii@gmail.com>
2022-01-16 12:59:57 -0300

    SEO changes / description and title

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Head from 'next/head';
 
 export default function SEO({
-  description = 'startup product landing page',
-  author = 'JSM',
+  description = 'Proyect made it with NextJs and React',
+  author = 'Lautaro Lazzeri',
   meta,
-  title = 'startup landing title',
+  title = 'Project made it with nextJs',
 }) {
   const metaData = [
     {
